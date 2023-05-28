eef4d3ccc099b91323b6e8bc3d5e9a0c2f2ad84d
Lesia Savchenko <les@MacBook-Air-Lesia.local>
2023-05-28 09:34:25 +0300

    добавлен winston

--- backend/app.js
+++ backend/app.js
@@ -5,6 +5,7 @@ const cors = require('cors');
 const helmet = require('helmet');
 const bodyParser = require('body-parser');
 const { rateLimiterUsingThirdParty } = require('./middlewares/rateLimit');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
 const router = require('./routes');
 const errorHandler = require('./middlewares/errorHandler');
 
@@ -13,10 +14,14 @@ mongoose.connect('mongodb://127.0.0.1:27017/mestodb');
 
 const app = express();
 app.use(cors());
+// подключаем логгер запросов
+app.use(requestLogger);
 app.use(bodyParser.json());
 app.use(helmet());
 app.use(rateLimiterUsingThirdParty);
 app.use(router);
+// подключаем логгер ошибок
+app.use(errorLogger);
 app.use(errors());
 
 app.use(errorHandler);
